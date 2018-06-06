a9c431f8af05690c097cf1522995e9001a3ef3ef
Andrew Doyle <andy.d.doyle@gmail.com>
2018-06-06 14:11:51 -0400

    Fixing square brackets to curly braces

--- src/main/java/com/iorthol/anathemachines/proxy/CommonProxy.java
+++ src/main/java/com/iorthol/anathemachines/proxy/CommonProxy.java
@@ -6,15 +6,15 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.internal.FMLProxyPacket;
 
 public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
 
-    public void preInit(FMLPreInitializationEvent event) [
-            ]
+    public void init(FMLInitializationEvent event) {
+        
+    }
 
-
-    public void init(FMLInitializationEvent event) [
-            ]
-
-    public void postInit(FMLPostInitializationEvent event) [
-            ]
-
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
 }
