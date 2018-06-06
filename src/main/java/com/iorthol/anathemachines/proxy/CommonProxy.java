34b5975ca8cf412d27538e04a2e21606bb871908
Iorthol <hippy263@msn.com>
2018-06-06 14:02:46 -0400

    AAAAAHHHH

---
+++ src/main/java/com/iorthol/anathemachines/proxy/CommonProxy.java
@@ -0,0 +1,20 @@
+package com.iorthol.anathemachines.proxy;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.internal.FMLProxyPacket;
+
+public class CommonProxy {
+
+    public void preInit(FMLPreInitializationEvent event) [
+            ]
+
+
+    public void init(FMLInitializationEvent event) [
+            ]
+
+    public void postInit(FMLPostInitializationEvent event) [
+            ]
+
+}
